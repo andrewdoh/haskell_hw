fa9eae9648dfb58f85ca178ab4fec373219f1e28
Andrew doh <dohandrew@protonmail.com>
2018-10-28 08:51:01 -0700

    finished hw2 section.

--- hw2/LogAnalysis.hs
+++ hw2/LogAnalysis.hs
@@ -29,10 +29,37 @@ parse s = map parseMessage $ lines s
 
 
 insert :: LogMessage -> MessageTree -> MessageTree
-insert logM mTree  = case logM of 
-                        (Unknown m)-> mTree
-                        _ -> (Node mTree logM mTree)
+insert logM mTree  = case logM of
+                                (Unknown _ ) -> mTree
+				(LogMessage mt ts m) -> case mTree of
+				       	     	     Leaf -> (Node Leaf logM Leaf)
+						     (Node l lm r) -> if ts <= time then (Node (insert logM l) lm r) else (Node l lm (insert logM r))
+						                      where time = getTime lm
+
+getTime :: LogMessage -> TimeStamp
+getTime (LogMessage _ ts _ ) = ts
+
+build :: [LogMessage] -> MessageTree
+build logM = case logM of 
+                [m] -> insert m Leaf
+                (m:ms) -> insert m $ build ms  
 
 
---build :: [LogMessage] -> MessageTree
---build 
+inOrder :: MessageTree -> [LogMessage]
+inOrder mt = case mt of 
+                (Node Leaf m Leaf) -> [m]
+		(Node Leaf m r) -> [m] ++ inOrder r
+		(Node l m Leaf) -> inOrder l ++ [m]
+		(Node l m r) -> inOrder l ++ [m] ++ inOrder r
+
+
+		
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong logMs = case logMs of
+                               ([m]) -> case m of
+			                         (LogMessage (Error l) _ m) -> if l >= 50 then [m] else []
+						 _ -> []
+			       (m:ms) ->  case m of
+			                         (LogMessage (Error l) _ m) -> if l >= 50 then m:whatWentWrong ms else whatWentWrong ms
+						 _ -> whatWentWrong ms 
+
