d82d924ec5c10a00b062c01db3886c48a6188483
Andrew doh <dohandrew@protonmail.com>
2018-10-29 21:07:20 -0700

    finished hw3.

--- hw3.hs
+++ hw3.hs
@@ -1,3 +1,4 @@
+import Data.List
 skips :: [a] -> [[a]]
 skips xs = [[fst y|y<-zip xs [1..length xs], (snd y) `mod` x == 0]| x<-[1..length xs]]
 
@@ -9,5 +10,5 @@ localMaxima xs = case xs of [] -> []
                             (a:b:c:[]) -> if b > a && b > c then [b] else []
                             (a:b:c:dz) -> if b > a && b > c then b : localMaxima (b:c:dz) else localMaxima (b:c:dz)
 
---histogram :: [Integer] -> String
---histogram (x:xs) = putStr
+histogram :: [Integer] -> String
+histogram xs = unlines [[ if elem y xs then '*' else ' '| y <- [0..9]] | xs <- reverse $ transpose $ group $ sort xs] ++ "==========\n0123456789\n"
