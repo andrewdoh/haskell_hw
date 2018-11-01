6880cf61c9d64c26d41af0dc668312bc65df4de7
Andrew doh <dohandrew@protonmail.com>
2018-10-31 21:19:45 -0700

    finished q1.

--- hw4.hs
+++ hw4.hs
@@ -1,2 +1,5 @@
-fun1` :: [Integer] -> Integer
-fun1` = product . map (\x -> x - 2) . filter even
+fun1 :: [Integer] -> Integer
+fun1 = product . map (\x -> x - 2) . filter even
+
+fun2 :: Integer -> Integer
+fun2 = sum . filter even . takeWhile (>1) . iterate (\x -> if even x && x /= 0 then x `div` 2 else if x == 1 then 0 else 3 * x  + 1)
