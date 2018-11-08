2ef978884a54afb277b057737a79a3b41cd0d642
Andrew doh <dohandrew@protonmail.com>
2018-11-07 19:40:06 -0800

    finished Q2H4.

--- hw4.hs
+++ hw4.hs
@@ -9,13 +9,6 @@ fun2 = sum . filter even . takeWhile (>1) . iterate (\x -> if even x && x /= 0 t
 
 foldTree :: [a] -> Tree a
 foldTree xs = foldr f Leaf xs
-
-d :: Tree a -> Bool
-d t = case t of
-             (Node _ Leaf _ Leaf) -> True
-	     (Node _ Leaf _ r)    -> True
-	     (Node _ l _ Leaf)    -> True
-	     _                    -> False
 			     
 f :: a -> Tree a -> Tree a
 f a b = case b of
@@ -23,7 +16,27 @@ f a b = case b of
 		(Node i Leaf m Leaf) -> (Node i (Node (i+1) Leaf a Leaf) m Leaf)
 	        (Node i l m Leaf) -> (Node i l m (Node (i+1) Leaf a Leaf))
 		(Node i Leaf m r) -> (Node i (Node (i+1) Leaf a Leaf) m r)
-		(Node i l m r) -> if d b then (Node i (f a l) m r) else (Node i l m (f a r))
-
+		(Node i l m r) -> if hl <= hr then (Node i (f a l) m r) else (Node i l m (f a r))
+		                       where hl = height l
+				             hr = height r
+					     
+height :: Tree a -> [Integer]
+height t = case t of
+                  Leaf -> [0]
+		  (Node i l _ r) -> i:(height l)++(height r)
+		  
 xor :: [Bool] -> Bool
 xor xs = foldr (\x y -> x /= y) False xs
+
+
+-- mapP :: (a -> b) -> [a] -> [b]
+-- mapP fa xs = foldr fa [] xs
+
+cartProd :: [a] -> [b] -> [(a,b)]
+cartProd xs ys = [(x,y) | x <- xs, y <-ys]
+
+cList :: Integer -> [Integer]
+cList n = [1..(2*n+2)]
+
+--sieveSundaram :: Integer -> [Integer]
+--sieveSundaram = filter(\x ->) cartProd [1..10] . cList
