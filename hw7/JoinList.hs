676cf16e37c108c7ca6b6abbd715da46d382a592
Andrew Do <dohandrew@protonmail.com>
2019-01-15 19:16:47 -0800

    clean up code and add takeJ. something feels off though just function again to ensure its properly working as intended.

--- hw7/JoinList.hs
+++ hw7/JoinList.hs
@@ -14,36 +14,51 @@ data JoinList m a = Empty
 
 tag :: Monoid m => JoinList m a -> m
 tag jl = case jl of
-           Empty -> mempty 
-           (Single m _ ) -> m
+           Empty           -> mempty 
+           (Single m _ )   -> m
            (Append m _ _ ) -> m
 
 indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
 indexJ i jl
             | i >= getSize (size $ tag jl) || i < 0 = Nothing
-            | otherwise            = case jl of
-                                         Empty -> Nothing
-                                         (Single m a) -> Just a
-                                         (Append _ l r) -> if i < lSize then indexJ i l else indexJ (i - lSize) r
+            | otherwise                             = case jl of
+                                                         Empty          -> Nothing
+                                                         (Single m a)   -> Just a
+                                                         (Append _ l r) -> if i < lSize then left  else right
                                                                        where
                                                                            lSize = getSize $ size $ tag l
+                                                                           left = indexJ i l
+                                                                           right = indexJ (i - lSize) r
 
 dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
 dropJ i jl
            | i > (getSize (size $ tag jl)) || i < 1 = jl
-           | otherwise                      = case jl of
-                                                Empty -> Empty
-                                                (Single m _ ) -> Empty
-                                                (Append m l r) -> if i <= lSize then left else right
-                                                                   where
-                                                                         lSize = getSize $ size $ tag l
-                                                                         rSize = getSize $ size $ tag r
-                                                                         nm    = tag l <> tag r
-                                                                         left  = Append nm (dropJ i l) r
-                                                                         right = Append nm Empty (dropJ (i - lSize) r)
+           | otherwise                              = case jl of
+                                                           Empty          -> Empty
+                                                           (Single m _ )  -> Empty
+                                                           (Append m l r) -> if i <= lSize then left else right
+                                                                              where
+                                                                                 lSize = getSize $ size $ tag l
+                                                                                 rSize = getSize $ size $ tag r
+                                                                                 nm    = tag l <> tag r
+                                                                                 left  = Append nm (dropJ i l) r
+                                                                                 right = Append nm Empty (dropJ (i - lSize) r)
 
 takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
-takeJ i jl = jl
+takeJ i jl
+           | i <= 0                          = Empty
+           | i >= (getSize ( size $ tag jl)) = jl
+           | otherwise                       = case jl of
+                                                 Empty          -> Empty
+                                                 (Single m d)   -> (Single m d)
+                                                 (Append m l r) -> if i <= lSize then left else right
+                                                                      where
+                                                                        lSize = getSize $ size $ tag l
+                                                                        rSize = getSize $ size $ tag r
+                                                                        nm    = tag l <> tag r
+                                                                        left  = Append nm (takeJ i l) Empty
+                                                                        right = Append nm l (takeJ (i - 1) r)
                                                                          
+
 
 
